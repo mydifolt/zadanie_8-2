df048258d963029e15826303533db1482954c2e2
Piotrek <mydifolt@gmail.com>
2017-05-12 13:56:22 +0200

    zadanie 8-2

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scripts.js file
+// There is a linear comment here, we can not use enter,
+// unless we do the next one again
+
+// alert('test');
+// the function above has been commented and will not be executed
+
+var imie = prompt('Wpisz prosze swoje imie');
+alert('Witaj, ' + imie);
+console.log('Witaj, ' + imie);
