0658ffef5a8a7d2c7fcca13e1e33dd8c3c01c7fc
Piotrek <mydifolt@gmail.com>
2017-05-14 14:57:40 +0200

    coments task 8-4

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,12 @@
 // the function above has been commented and will not be executed
 
 var imie = prompt('Wpisz prosze swoje imie');
-alert('Witaj, ' + imie);
+// alert('Witaj, ' + imie);
+// dowolnym typem komentarza
 console.log('Witaj, ' + imie);
+//Dodaję jeszcze dwa inne komentarze.
+/* Stworzyłem nowe repozytorium i wysłałem tam swój projekt. 
+Pamiętam o udostępnieniu go Mentorowi na Githubie.
+Link do projektu wstawiłem na końcu zadania.
+*/
+ 
